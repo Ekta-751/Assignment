f8bba656008f2c1d78c5fdf8c079d2917c94e0b0
Ekta_751 <ektav782@gmail.com>
2018-06-12 15:56:21 +0530

    first command

---
+++ verma.py
@@ -0,0 +1,51 @@
+x=5
+print(x)
+print(type(x))
+
+x=15
+print(x)
+print(type(x))
+
+x='a'
+print(x)
+print(type(x))
+
+x='abcdef'
+print(x)
+print(type(x))
+
+x=5+10J
+print(x)
+print(type(x))
+
+x=5+10J
+x=x+5
+print(x)
+print(type(x))
+
+x="Hello World"
+print(x)
+print(type(x))
+
+x="Hello 'World'"
+print(x)
+print(type(x))
+
+x='Hello "World"'
+print(x)
+print(type(x))
+
+x='Hello \"World\"'
+print(x)
+print(type(x))
+
+x="Hello \"World\""
+print(x)
+print(type(x))
+
+x="Hello \"World\""
+print(x)
+print(type(x))
+
+
+

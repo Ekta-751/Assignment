df19ca2591e2fc521854e0d696cd29704fe3fece
Ekta_751 <ektav782@gmail.com>
2018-06-13 15:54:41 +0530

    file added

---
+++ assignment3.py
@@ -0,0 +1,49 @@
+#create a list with user defined inputs.
+print ("enter the list item")
+list=[]
+x=input("2")
+y=input("3")
+z=input("4")
+print("the list is")
+list=[x,y,z]
+print(list)
+print("\n")
+
+#Add the following list the above create list{'googal','apple',facebook','microsoft','tesla']
+list2=['googal','apple','facebook','microsoft','tesla']
+print(list2)
+print(list.extend(list2))
+print(list)
+print("\n")
+
+#count  the number of time on object
+list=[1,2,3,2,4,2,5,6,7,2]
+list.count(2)
+print("\n")
+
+#create a list with number and sort it in ascending order.
+l=[1,3,4,6,3,5]
+print(l.sort())
+print(l)
+print("\n")
+
+#question(5)
+list3=[1,2,5,3,6,4]
+print(list3)
+list4=[7,9,11,8,10,12]
+print(list4)
+list3.sort()
+list4.sort()
+print(list3)
+print(list4)
+print("\n")
+p1=[list3,list4]
+print(p1)
+print("\n")
+
+
+
+
+ 
+
+
